c4e87f66becf31ac9af2b26fa53dcc85059503c8
sikara <xuhaoxian@gdlinkjob.com>
2024-02-23 16:27:27 +0800

    add nativewind

--- App.tsx
+++ App.tsx
@@ -33,15 +33,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
+      <Text className="text-2xl text-black dark:text-white">{title}</Text>
       <Text
         style={[
           styles.sectionDescription,
